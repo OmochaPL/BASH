126b8daa05123cd49c6b10777975e83799a7806a
OmochaPL <94681221+OmochaPL@users.noreply.github.com>
2022-02-22 13:30:18 +0100

    Create CaseExampleCars.sh

---
+++ CaseExampleCars.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CAR=BMW
+case $CAR in
+    Toyota)
+        echo "it's Toyota"
+        ;;
+    Honda)
+        echo "it's Honda"
+        ;;
+    BMW)
+        echo "it's BMW"
+        ;;
+    *)
+        echo "it's another car?"
+        ;;
+esac
+
