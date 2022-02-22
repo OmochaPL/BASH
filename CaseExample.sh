aadc1034522b9d2313aa63605af0de69b43a6856
OmochaPL <94681221+OmochaPL@users.noreply.github.com>
2022-02-22 13:02:32 +0100

    Create CaseExample.sh

---
+++ CaseExample.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+VAR=10
+case $VAR in
+    10)
+        echo "it's 10"
+        ;;
+    20)
+        echo "it's 20"
+        ;;
+    30)
+        echo "it's 30"
+        ;;
+    *)
+        echo "it's something else"
+        ;;
+
+esac
